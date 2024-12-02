a0a0e34f12a7d27848b1eb0e0859c0b33ed691c4
sans1075 <148881010+sans1075@users.noreply.github.com>
2024-12-02 11:51:53 -0500

    Update passwordgenerator.ps1

--- passwordgenerator.ps1
+++ passwordgenerator.ps1
@@ -1,5 +1,11 @@
+Write-Host "Warning: This script will change the password of the user you input." -ForegroundColor Red
+Write-Host "Enter the username:"
+$user = Read-Host
 $chars = @("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
            "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
            "1","2","3","4","5","6","7","8","9","0",
-           "!","@","#","`$","%","^","&","*","(",")","_","-","+","{","}","[","]","|","<",">","?","=","~","\","/",",",".","``")
-Write-Output $chars | Get-Random -Count 18 | Join-String
+           "!","@","#","`$","%","^","&","*","(",")","_","-","+","{","}","[","]","|","<",">","?","=","~","\","/",",",".")
+$password = $chars | Get-Random -Count 18 | Join-String
+Get-LocalUser -Name "$user"
+Set-LocalUser -Password "$password"
+Write-Output "$user's New Password: $password"
